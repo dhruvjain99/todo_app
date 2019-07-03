a23bcc911ac8f0b4780dfb96d6f7a849afc701d6
djcruz93 <dhruv.jain93@gmail.com>
2019-07-03 14:39:32 +0530

    add a home controller to render the home page

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const port = 9000;
 var app = express();
 
+const homeController = require('./controllers/home_controller');
+
+
+app.get('/', homeController.home);
+
 app.listen(port, function(err){
     if(err){
         console.log("Error in starting the server :: ", err);
